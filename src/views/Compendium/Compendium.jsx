91a369301d0c600a4f7261d430ec4c1659d7e844
‘Katie <ktschr11@gmail.com>
2021-12-06 10:15:01 -0800

    update for netlify build

--- src/views/Compendium/Compendium.jsx
+++ src/views/Compendium/Compendium.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { fetchGenres, fetchShows, fetchSearchedShow } from '../../services/shows.js';
 import Controls from '../../components/Controls/Controls';
--- src/views/Compendium/Compendium.test.jsx
+++ src/views/Compendium/Compendium.test.jsx
@@ -1,15 +1,13 @@
 import { screen, render } from '@testing-library/react'
 import Compendium from './Compendium'
 
-it ('should fetch shows, then render the list', async () => {
+it('should fetch shows, then render the list', async () => {
   render(<Compendium />)
 
   const loading = screen.getByText(/loading/)
   expect(loading).toBeInTheDocument()
 
-  const shows = await screen.findByRole('*', {
-    name: 'showList'
-  },);
+  const shows = await screen.findByRole('Compendium-Container');
 
   expect(shows).toMatchSnapshot();
 })
