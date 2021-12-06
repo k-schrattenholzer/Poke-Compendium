cabfad72fe3f7cdeb13d845a4d3254dd515a7679
‘Katie <ktschr11@gmail.com>
2021-12-05 18:36:27 -0800

    added Show test

--- src/components/Show/Show.test.jsx
+++ src/components/Show/Show.test.jsx
@@ -3,15 +3,20 @@ import Show from './Show.jsx';
 
 it('should render one show component', () => {
   render (
-    <Show show={{
-      id: 6,
-    name:'What We Do in the Shadows',
-    lang: "English",
-    premiere: '01*01*01',
-    image: 'https://assets.fxnetworks.com/cms/prod/2021/07/22/web_poster_540x796.jpg',
-    summary: 'a rag tag group learns the meaning of christmas',
-    url: 'https://assets.fxnetworks.com/cms/prod/2021/07/22/web_poster_540x796.jpg',
-    genres: ['horror', 'thriller', 'family']
-    }}/>
+    <Show
+      show={{
+        id: 6,
+        name:'What We Do in the Shadows',
+        lang: "English",
+        premiere: '01/01/01',
+        image: 'https://assets.fxnetworks.com/cms/prod/2021/07/22/web_poster_540x796.jpg',
+        summary: 'a rag tag group learns the meaning of christmas',
+        url: 'https://assets.fxnetworks.com/cms/prod/2021/07/22/web_poster_540x796.jpg',
+        genres: ['horror', 'romcom', 'family']
+      }}
+    />
   )
+
+  const show = screen.getByText(/Shadows/);
+  expect(show).toMatchSnapshot()
 })
